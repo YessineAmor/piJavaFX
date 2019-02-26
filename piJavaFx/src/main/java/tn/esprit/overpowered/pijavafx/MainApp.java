42292a9c2b878979427f894a7a052298341f0458
YessineAmor <yassine.amor@esprit.tn>
2019-02-26 18:42:18 +0100

    Initialized the project

---
+++ piJavaFx/src/main/java/tn/esprit/overpowered/pijavafx/MainApp.java
@@ -0,0 +1,50 @@
+package tn.esprit.overpowered.pijavafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tn.esprit.overpowered.byusforus.entities.Choice;
+import tn.esprit.overpowered.byusforus.services.ChoiceFacadeRemote;
+
+public class MainApp extends Application {
+
+    private static final Logger log = LoggerFactory.getLogger(MainApp.class);
+
+    public static void main(String[] args) throws Exception {
+        String jndiName = "piJEE-ejb-1.0/ChoiceFacade!tn.esprit.overpowered.byusforus.services.ChoiceFacadeRemote";
+        Context context = new InitialContext();
+        ChoiceFacadeRemote choiceFacadeProxy = (ChoiceFacadeRemote) context.lookup(jndiName);
+        System.out.println("Starting choice creation...");
+        Choice choice1 = new Choice();
+        choice1.setChoicePoints(1);
+        choice1.setChoiceText("Your choice");
+        choice1.setIsCorrectChoice(Boolean.TRUE);
+        choiceFacadeProxy.create(choice1);
+        System.out.println("Completed choice creation");
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+
+        log.info("Starting Hello JavaFX and Maven demonstration application");
+
+        String fxmlFile = "/fxml/hello.fxml";
+        log.debug("Loading FXML for main view from: {}", fxmlFile);
+        FXMLLoader loader = new FXMLLoader();
+        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+
+        log.debug("Showing JFX scene");
+        Scene scene = new Scene(rootNode, 400, 200);
+        scene.getStylesheets().add("/styles/styles.css");
+
+        stage.setTitle("Hello JavaFX and Maven");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
